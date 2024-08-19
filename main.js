8476273613ac7aee3530f9dad3c4af95a8ab0a87
LorisSigrist <loris@sigrist.dev>
2024-08-19 11:51:13 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import * as m from "./messages.js"
+
+// __YEAR__ is defined in vite.config.js
+console.log(m[`new_year_${__YEAR__}`]())
---
+++ messages.js
@@ -0,0 +1,3 @@
+export const new_year_2020 = () => "Hello 2020"
+export const new_year_2024 = () => "Hello 2024"
+export const other = () => "Other"
